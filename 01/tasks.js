08b3017ab6f046fc226292150f2a4700bb272678
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-09-28 17:07:55 +0300

    Minor_bug_fixes_2_task3

--- 01/tasks.js
+++ 01/tasks.js
@@ -43,9 +43,10 @@ const cache = [0, 1];
 
 function fibonacciWithCache(x) {
   let result = cache[x];
+  
   if (typeof result !== 'number') {
-      result = fibonacciWithCache(x-1) + fibonacciWithCache(x-2);
-      cache[x] = result;
+    result = fibonacciWithCache(x - 1) + fibonacciWithCache(x - 2);
+    cache[x] = result;
   }
   return result;
 }
